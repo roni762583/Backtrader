cda238b6da4bf4706ed3d0753f58a5a50c7afb52
Aharon Zbaida <roni762583@gmail.com>
2020-12-01 07:21:26 +0200

    continued with tutorial until geting stuck on bug:
    if order.status in order.Completed:...  during video 3
    
    https://youtu.be/K8buXUxEfMc

--- trader.py
+++ trader.py
@@ -1,6 +1,26 @@
 import backtrader
+import datetime
+from strategies import TestStrategy
 
 cerebro = backtrader.Cerebro()
+
+cerebro.broker.set_cash(1000000)
+
+ # Create a Data Feed
+data = backtrader.feeds.YahooFinanceCSVData(
+    dataname='oracle.csv',
+    # Do not pass values before this date
+    fromdate=datetime.datetime(2000, 1, 1),
+    # Do not pass values before this date
+    todate=datetime.datetime(2000, 12, 31),
+    # Do not pass values after this date
+    reverse=False)
+
+# add Strategy
+cerebro.addstrategy(TestStrategy)
+
+# add data
+cerebro.adddata(data)
 
 print('starting portfoilio value: ' + str(cerebro.broker.getvalue()))
 
